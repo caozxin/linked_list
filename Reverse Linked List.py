a1c12fec7be81cb9388cf85323f5fff110abbcb1
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-04 10:33:21 -0500

    Update Reverse Linked List.py

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -15,15 +15,35 @@ class Solution:
     @param head: n
     @return: The new head of reversed linked list.
     """
-    def reverse(self, head: ListNode) -> ListNode:
-        # write your code here
+    def reverse(self, head: ListNode) -> ListNode: # recursive approach
+        if not head or not head.next:# base case
+            return head
 
-        reversed_head = None 
-        while head != None:
-            #set vars:
-            temp = head.next # this is a node
-            head.next = reversed_head
-            reversed_head = head
-            head = temp
-            
-        return reversed_head
+        reversed = self.reverse(head.next) # recurse everything after current ListNode
+
+        head.next.next = head; # operations on the current ListNode, so that current linking will be nullafiled and backward linking will be added
+        head.next = None
+
+        return reversed
+
+
+    def reverse_default(self, head: ListNode) -> ListNode:
+        # write your code here
+        curr_list = []
+        # curr_val = None
+        def linked_list_traversal(head:ListNode): 
+            if not head:
+                return curr_list
+            curr_val = head.val
+            print(curr_val)
+            curr_list.append(curr_val)
+            head.val = None
+            # curr_val = val
+            next = self.reverse(head.next)
+            head.val = curr_val
+        
+
+        result_list = linked_list_traversal(head)
+        print("curr_list", curr_list)
+
+        return head
