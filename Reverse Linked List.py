58578d16cf9057c0ed583fe4bcc5a855028c9a97
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-04 04:08:36 -0500

    Update Reverse Linked List.py

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -1,5 +1,3 @@
-#Reverse a linked list.
-
 from lintcode import (
     ListNode,
 )
@@ -19,25 +17,13 @@ class Solution:
     """
     def reverse(self, head: ListNode) -> ListNode:
         # write your code here
-        curr_list = []
-        # curr_val = None
-        def linked_list_traversal(head:ListNode): 
-            if not head:
-                return curr_list
-            curr_val = head.val
-            print(curr_val)
-            curr_list.append(curr_val)
-            head.val = None
-            # curr_val = val
-            next = self.reverse(head.next)
-            head.val = curr_val
-        
 
-        result_list = linked_list_traversal(head)
-        print("curr_list", curr_list)
-        # print("curr_val", curr_val)
-        # head.val = curr_list[0]
-        # if curr_list:
-        #     head.val = curr_list[0]
-        #     print(head.val)
-        return head
+        reversed_head = None 
+        while head != None:
+            #set vars:
+            temp = head.next # this is a node
+            head.next = reversed_head
+            reversed_head = head
+            head = temp
+            
+        return reversed_head
