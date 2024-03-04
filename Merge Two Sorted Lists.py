78f374eb50d62aa2805bf40aae19262e1849f95e
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-04 10:48:08 -0500

    Update Merge Two Sorted Lists.py

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -1,1 +1,21 @@
 
+from lintcode import (
+    ListNode,
+)
+
+"""
+Definition of ListNode:
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param l1: ListNode l1 is the head of the linked list
+    @param l2: ListNode l2 is the head of the linked list
+    @return: ListNode head of linked list
+    """
+    def merge_two_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # write your code here
