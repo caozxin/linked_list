17838a2d9c96155484419552fa4fba549db4fad6
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-04 22:24:52 -0500

    Update Merge Two Sorted Lists.py

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -1,4 +1,3 @@
-
 from lintcode import (
     ListNode,
 )
@@ -19,3 +18,20 @@ class Solution:
     """
     def merge_two_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
         # write your code here
+
+        #we start with insert operation:
+        if not l1 or not l2:#base case to exit
+            return
+        print(l1.val, l2.val)
+
+        self.merge_two_lists(l1, l2.next) # recurse everything after current ListNode
+
+        if l1.val >= l2.val: # operations on the current ListNode
+            print("operation starts: ", l1.val, l2.val)
+            temp = l2.next #(temp = l2.next = '2')
+            l1.next = temp #(l1.next = '2')
+            l2.next = l1 #(l2.next = '0'.next = l1)
+
+        return l2
+        
+
