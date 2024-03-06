a81abad3da2432ea8916ee72ce93c473af24586e
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-06 10:16:15 -0500

    Update Remove Nth Node From End of List.py

--- Remove Nth Node From End of List.py
+++ Remove Nth Node From End of List.py
@@ -1,1 +1,30 @@
-#Given a linked list, remove the nth node from the end of list and return its head.
+from lintcode import (
+    ListNode,
+)
+
+"""
+Definition of ListNode:
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @param n: An integer
+    @return: The head of linked list.
+    """
+    def remove_nth_from_end(self, head: ListNode, n: int) -> ListNode:
+        # write your code here
+
+        if not head: # base case
+            
+            return head
+
+        self.remove_nth_from_end(head.next, n)
+        print(head.val)
+        num += 1
+
+        return head
