282065c458e06c34cca3f18a83d69ed10a043621
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-09 20:58:22 -0500

    Update Remove Nth Node From End of List.py

--- Remove Nth Node From End of List.py
+++ Remove Nth Node From End of List.py
@@ -1,15 +1,3 @@
-from lintcode import (
-    ListNode,
-)
-
-"""
-Definition of ListNode:
-class ListNode(object):
-    def __init__(self, val, next=None):
-        self.val = val
-        self.next = next
-"""
-
 class Solution:
     """
     @param head: The first node of linked list.
@@ -17,27 +5,22 @@ class Solution:
     @return: The head of linked list.
     """
     def remove_nth_from_end(self, head: ListNode, n: int) -> ListNode:
-        # write your code here
-        abs_pos = -n
-        # we need to keep track of the previous node to conduct the deletion: 
-
-        def traversal(pre_head, head, n, abs_pos):
-            if not head: # base case
-                return head
-            n -= 1
-            # abs_pos -= 1
-            if pre_head: 
-                print(" ", pre_head.val, head.val, n, abs_pos)
-            # self.remove_nth_from_end(head.next, n)
-            traversal(head, head.next, n, abs_pos)
-
-            if n == abs_pos:
-                print("found it!: ", pre_head.val, head.val)
-                #start deleltion of the current node: --> we should change the current head
-                temp = head.next # (head.next = 5)
-                head = pre_head
-                head.next = temp
+        # Define a dummy node to handle edge cases
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
         
-            return head
-        traversal(None, head, n, abs_pos)
-        return head
+        # Move the first pointer ahead by n+1 steps
+        for i in range(n + 1):
+            first = first.next
+        
+        # Move both pointers together until the first pointer reaches the end
+        while first is not None:
+            first = first.next
+            second = second.next
+        
+        # Remove the nth node from the end
+        second.next = second.next.next
+        
+        return dummy.next
