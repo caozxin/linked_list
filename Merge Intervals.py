461d238785b005dae8f977b715c7b73e89a2434a
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-19 02:44:06 -0400

    Create Merge Intervals.py

---
+++ Merge Intervals.py
@@ -0,0 +1,1 @@
+#We take a list intervals of type Interval to represent a collection of intervals, where a single interval is (start, end). You need to merge all overlapping intervals and return an array of non-overlapping intervals that covers exactly all intervals in the input.
