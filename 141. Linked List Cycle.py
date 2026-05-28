a97f9bceb2bd2ad7eee639f40509ddfc095f0370
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2026-05-28 07:20:59 -0400

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -19,3 +19,27 @@ class Solution:
                 return True
 
         return False
+
+
+### best version:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+
+        dummy = head
+        slow, fast = dummy, dummy
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
