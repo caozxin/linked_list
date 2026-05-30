c07c6aafc69fab33dd0961b48a007479d1a1c1dd
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2026-05-30 07:02:28 -0400

    Update 287. Find the Duplicate Number.py

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -1,19 +1,22 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow = nums[0]
-        fast = nums[0]
+        
+
+        fast, slow = nums[0], nums[0] # we initiate both pointers to 1 now
 
         while True:
+            fast = nums[nums[fast]] # we use the fact that indx and val are between [1, n] to move 2 steps for fast and 1 step for slow
             slow = nums[slow]
-            fast = nums[nums[fast]]
 
-            if slow == fast:
+            # print("fast, slow", fast, slow)
+
+            if fast == slow:
                 break
 
-        slow = nums[0]
+        slow = nums[0] # we reset slow at the beginnging and keep fast at last meeting place
 
-        while slow != fast:
+        while fast != slow: # move fast and slow 1 step at one time when them meet
             slow = nums[slow]
             fast = nums[fast]
 
-        return slow
+        return slow # then wherever they met is the dups
